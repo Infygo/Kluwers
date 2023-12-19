1e57bd8854ca245e1c5a12e39b04b2d1fee78164
vignesh <vigneshwaran89@gmail.com>
2023-12-19 09:54:46 +0100

    Framework Skeleton - pomxml- featurefiles

---
+++ ToDo/src/main/java/Wolters/ToDo/App.java
@@ -0,0 +1,13 @@
+package Wolters.ToDo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
